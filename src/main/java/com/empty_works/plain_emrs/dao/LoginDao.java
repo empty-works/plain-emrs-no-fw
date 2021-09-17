b0caea5f2f6bdac29ca9daa7ed16f733afa9e120
empty-works <pama.mark@gmail.com>
2021-09-17 22:56:30 +0200

    Login controller tentatively complete. Could improve method to get role so no need to change too much if roles change in the future.

--- src/main/java/com/empty_works/plain_emrs/dao/LoginDao.java
+++ src/main/java/com/empty_works/plain_emrs/dao/LoginDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.empty_works.plain_emrs.beans.LoginBean;
+import com.empty_works.plain_emrs.contants.PlainEmrsRole;
 import com.empty_works.plain_emrs.util.ConnectionUtil;
 
 public class LoginDao {
@@ -27,12 +28,25 @@ public class LoginDao {
 				
 				String usernameDb = rs.getString("username");
 				String passwordDb = rs.getString("password");
+				String roleDb = rs.getString("authority");
 				
-				
+				if(usernameDb.equals(username) && passwordDb.equals(password)) 
+					return getRole(roleDb);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return "Invalid user";
+	}
+	
+	private String getRole(String roleDb) {
+		
+		if(roleDb.equals(PlainEmrsRole.admin)) return PlainEmrsRole.admin;
+		else if(roleDb.equals(PlainEmrsRole.doctor)) return PlainEmrsRole.doctor;
+		else if(roleDb.equals(PlainEmrsRole.nurse)) return PlainEmrsRole.nurse;
+		else if(roleDb.equals(PlainEmrsRole.alliedMedStaff)) return PlainEmrsRole.alliedMedStaff;
+		else if(roleDb.equals(PlainEmrsRole.provider)) return PlainEmrsRole.provider;
+		else if(roleDb.equals(PlainEmrsRole.government)) return PlainEmrsRole.government;
+		else return PlainEmrsRole.unknown;
 	}
 }
