5da71b9835d8d4d32aa476f9163e3b8fd0d0dfe3
empty-works <pama.mark@gmail.com>
2021-09-17 05:12:34 +0200

    Created User bean.

--- src/main/java/com/empty_works/plain_emrs/beans/User.java
+++ src/main/java/com/empty_works/plain_emrs/beans/User.java
@@ -1,5 +1,57 @@
 package com.empty_works.plain_emrs.beans;
 
+import java.util.Date;
+
 public class User {
 
+	private String username;
+	private String password;
+	private String emailAddress;
+	private boolean enabled;
+	private Date createdOn;
+	private int patientId;
+	private int nonpatientId;
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public int getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+	public int getNonpatientId() {
+		return nonpatientId;
+	}
+	public void setNonpatientId(int nonpatientId) {
+		this.nonpatientId = nonpatientId;
+	}
 }
