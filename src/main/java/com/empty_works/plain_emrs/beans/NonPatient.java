882bfb91942bd207a973b70f4584af064eddf250
empty-works <pama.mark@gmail.com>
2021-09-21 22:43:51 +0200

    Created non-patient bean and servlet.

---
+++ src/main/java/com/empty_works/plain_emrs/beans/NonPatient.java
@@ -0,0 +1,50 @@
+package com.empty_works.plain_emrs.beans;
+
+import java.util.Date;
+
+public class NonPatient {
+
+	private String givenName;
+	private String middleName;
+	private String lastName;
+	private String organization;
+	private Date dateOfBirth;
+	private String description;
+
+	public String getGivenName() {
+		return givenName;
+	}
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getOrganization() {
+		return organization;
+	}
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
