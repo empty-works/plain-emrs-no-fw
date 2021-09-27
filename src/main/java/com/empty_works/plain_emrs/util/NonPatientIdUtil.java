38bb82ce713a70d24cdc6a314cce76308ed39e7e
empty-works <pama.mark@gmail.com>
2021-09-27 22:41:44 +0200

    Completed the get name portion of the non-patient ID.

--- src/main/java/com/empty_works/plain_emrs/util/NonPatientIdUtil.java
+++ src/main/java/com/empty_works/plain_emrs/util/NonPatientIdUtil.java
@@ -6,13 +6,14 @@ final public class NonPatientIdUtil {
 
 	final public static String get(NonPatientBean np) {
 		
-		StringBuilder id = new StringBuilder("PENP-");
-		
+		StringBuilder idSb = new StringBuilder("PENP-");
 
-		return id.toString();
+		idSb.append(getNameId(np.getGivenName(), np.getLastName()));
+		
+		return idSb.toString();
 	}
 	
-	private static String nameId(String givenName, String lastName) {
+	private static String getNameId(String givenName, String lastName) {
 		
 		String lowercase = "abcdefghijklmnopqrstuvwxyz";
 		String gnSub = getNameSub(givenName);
@@ -20,19 +21,31 @@ final public class NonPatientIdUtil {
 		
 		StringBuilder namesSb = new StringBuilder("");
 
-		int[] nameNums = new int[4];
-		namesSb.append(lowercase.indexOf(Character.toLowerCase(gnSub.charAt(0))));
-		if(gnSub.length() < 1) namesSb.append(lowercase.indexOf(Character.toLowerCase(gnSub.charAt(1))));
+		namesSb.append(normalizeNum(lowercase.indexOf(Character.toLowerCase(gnSub.charAt(0)))));
+		if(gnSub.length() > 1) namesSb.append(lowercase.indexOf(Character.toLowerCase(gnSub.charAt(1))));
+		else namesSb.append("00");
 		namesSb.append(lowercase.indexOf(Character.toLowerCase(lnSub.charAt(0))));
-		if(lnSub.length() < 1) namesSb.append(lowercase.indexOf(Character.toLowerCase(lnSub.charAt(1))));
+		if(lnSub.length() > 1) namesSb.append(lowercase.indexOf(Character.toLowerCase(lnSub.charAt(1))));
+		else namesSb.append("00");
 		
+		System.out.println("Name portion of non-patient ID: " + namesSb.toString());
 		
+		return namesSb.toString();
 	}
 	
+	// getNameId helper
+	private static String normalizeNum(int index) {
+		
+		int DOUBLE_DIGITS = 10;
+		if(index < DOUBLE_DIGITS) {
+			return "0" + index;
+		}
+		return "" + index;
+	}
+	
+	// getNameId helper
 	private static String getNameSub(String name) {
 		
 		return (name.length() > 1) ? "" + name.charAt(0) + name.charAt(1) : "" + name.charAt(0);
 	}
-	
-	
 }
