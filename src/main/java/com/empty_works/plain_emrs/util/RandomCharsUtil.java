6a9a09e176149a7724aeced121b8f16290368930
empty-works <pama.mark@gmail.com>
2021-10-04 22:23:42 +0200

    Added more methods to allow for different combinations of characters.

--- src/main/java/com/empty_works/plain_emrs/util/RandomCharsUtil.java
+++ src/main/java/com/empty_works/plain_emrs/util/RandomCharsUtil.java
@@ -10,7 +10,51 @@ public class RandomCharsUtil {
 	
 	final public static String getLetters(int length) {
 		
-		return scramble(length, letters);
+		String seq = scramble(length, letters);
+		System.out.println("RandomCharsUtil.getLetters=" + seq);
+		return seq;
+	}
+	
+	final public static String getNums(int length) {
+		
+		String seq = scramble(length, nums);
+		System.out.println("RandomCharsUtil.getNums=" + seq);
+		return seq;
+	}
+	
+	final public static String getSymbols(int length) {
+		
+		String seq = scramble(length, symbols);
+		System.out.println("RandomCharsUtil.getSymbols=" + seq);
+		return seq;
+	}
+	
+	final public static String getLettersNums(int length) {
+		
+		String seq = scramble(length, letters, nums);
+		System.out.println("RandomCharsUtil.getLettersNums=" + seq);
+		return seq;
+	}
+	
+	final public static String getLettersSymbols(int length) {
+		
+		String seq = scramble(length, letters, symbols);
+		System.out.println("RandomCharsUtil.getLettersSymbols=" + seq);
+		return seq;
+	}
+
+	final public static String getLettersNumsSymbols(int length) {
+		
+		String seq = scramble(length, letters, nums, symbols);
+		System.out.println("RandomCharsUtil.getLettersNumsSymbols=" + seq);
+		return seq;
+	}
+
+	final public static String getNumsSymbols(int length) {
+		
+		String seq = scramble(length, letters, symbols);
+		System.out.println("RandomCharsUtil.getLettersSymbols=" + seq);
+		return seq;
 	}
 	
 	private static String scramble(int length, String... chars) {
@@ -31,7 +75,6 @@ public class RandomCharsUtil {
 			sb.append(comboSeq.charAt(rand.nextInt(comboSeq.length())));
 		}
 		
-		System.out.println("RandomCharUtil scramble()= " + sb.toString());
 		return sb.toString();
 	}
 }
