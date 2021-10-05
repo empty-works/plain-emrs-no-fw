39268cfdc955ed5c2b45bfc7ad22923fd613a528
empty-works <pama.mark@gmail.com>
2021-10-05 22:38:24 +0200

    Added a couple more tests to the random character generator unit test.

--- src/test/java/com/empty_works/plain_emrs/util/RandomCharsUtilTest.java
+++ src/test/java/com/empty_works/plain_emrs/util/RandomCharsUtilTest.java
@@ -6,11 +6,19 @@ import org.junit.jupiter.api.Assertions;
 public class RandomCharsUtilTest {
 
 	@Test
-	void testGetLetters() {
+	void testGetLetters_sequenceLength() {
 		
 		String sequence = RandomCharsUtil.getLetters(5);
 		Assertions.assertEquals(5, sequence.length());
-		Assertions.assertEquals(true, Character.isAlphabetic(sequence.charAt(0)));
-
 	}
+	
+	@Test
+	void testGetLetters_containsAlphabetic() {
+		
+		String sequence = RandomCharsUtil.getLetters(5);
+		Assertions.assertEquals(true, Character.isAlphabetic(sequence.charAt(0)));
+	}
+	
+	@Test
+	void 
 }
