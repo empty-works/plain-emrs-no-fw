634b86cdd5eaddd440192f925b5041dd861b6e24
empty-works <pama.mark@gmail.com>
2021-10-06 20:51:27 +0200

    Made small corrections to unit test.

--- src/test/java/com/empty_works/plain_emrs/util/RandomCharsUtilTest.java
+++ src/test/java/com/empty_works/plain_emrs/util/RandomCharsUtilTest.java
@@ -9,16 +9,26 @@ public class RandomCharsUtilTest {
 	void testGetLetters_sequenceLength() {
 		
 		String sequence = RandomCharsUtil.getLetters(5);
+		System.out.println("Test if length is 5 characters long= " + sequence);
 		Assertions.assertEquals(5, sequence.length());
 	}
 	
 	@Test
-	void testGetLetters_containsAlphabetic() {
+	void testGetLetters_containsAllAlphabetic() {
 		
 		String sequence = RandomCharsUtil.getLetters(5);
-		Assertions.assertEquals(true, Character.isAlphabetic(sequence.charAt(0)));
+		boolean isAlphabetic = true;
+		for(int i = 0; i < sequence.length(); i++) {
+			
+			if(!Character.isAlphabetic(sequence.charAt(i))) {
+				
+				isAlphabetic = false;
+				break;
+			}
+		}
+		System.out.println("Test if all characters are alphabetic= " + sequence);
+		Assertions.assertEquals(true, isAlphabetic);
 	}
 	
-	@Test
-	void 
+	
 }
