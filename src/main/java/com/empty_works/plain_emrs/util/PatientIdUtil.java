1b824ed3a9bdadfc37f8c7ffdd97b98f1c9b6f87
empty-works <pama.mark@gmail.com>
2021-10-09 19:21:07 +0200

    The patient ID utility class now invokes the ID helper utility.

--- src/main/java/com/empty_works/plain_emrs/util/PatientIdUtil.java
+++ src/main/java/com/empty_works/plain_emrs/util/PatientIdUtil.java
@@ -1,5 +1,12 @@
 package com.empty_works.plain_emrs.util;
 
+import com.empty_works.plain_emrs.beans.PatientBean;
+import com.empty_works.plain_emrs.util.helpers.IdUtil;
+
 public class PatientIdUtil {
 
+	final public static String get(PatientBean pb) {
+		
+		return "PEP-" + IdUtil.get(pb);
+	}
 }
