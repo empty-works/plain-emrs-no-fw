d7f44066260147b1a88453703b7d437642808678
empty-works <pama.mark@gmail.com>
2021-10-11 21:39:10 +0200

    Created DAOs for registering non-patient and patient users.

---
+++ src/main/java/com/empty_works/plain_emrs/dao/RegisterNonPatientUserDao.java
@@ -0,0 +1,12 @@
+package com.empty_works.plain_emrs.dao;
+
+import com.empty_works.plain_emrs.beans.NonPatientBean;
+
+public class RegisterNonPatientUserDao {
+
+	//TODO: write to database. FIRST, update database
+	public String register(NonPatientBean npb) {
+		
+		return "";
+	}
+}
---
+++ src/main/java/com/empty_works/plain_emrs/dao/RegisterPatientDao.java
@@ -0,0 +1,17 @@
+package com.empty_works.plain_emrs.dao;
+
+import com.empty_works.plain_emrs.beans.PatientBean;
+
+public class RegisterPatientDao {
+
+	//TODO: write to database. First, update database
+	public String register(PatientBean pb) {
+		
+		String givenName = pb.getGivenName();
+		String middleName = pb.getMiddleName();
+		String lastName = pb.getLastName();
+		String 
+		
+		return "";
+	}
+}
