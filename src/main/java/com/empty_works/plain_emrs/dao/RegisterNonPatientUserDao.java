441680e29e0d35f6461dc63f92c319a10a0f6a4c
empty-works <pama.mark@gmail.com>
2021-10-12 22:54:16 +0200

    The non-patient DAO now connects and writes to the database.

--- src/main/java/com/empty_works/plain_emrs/dao/RegisterNonPatientUserDao.java
+++ src/main/java/com/empty_works/plain_emrs/dao/RegisterNonPatientUserDao.java
@@ -1,12 +1,18 @@
 package com.empty_works.plain_emrs.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 import com.empty_works.plain_emrs.beans.NonPatientBean;
+import com.empty_works.plain_emrs.util.ConnectionUtil;
 
 public class RegisterNonPatientUserDao {
 
-	//TODO: write to database. FIRST, update database
+	final public static String NONPATIENTDAO_SUCCESS = "SUCCESS";
+	
 	public String register(NonPatientBean npb) {
 		
 		String id = npb.getId();
@@ -20,7 +26,32 @@ public class RegisterNonPatientUserDao {
 		String staffPositionId = npb.getStaffPositionId();
 		String specialtyId = npb.getSpecialtyId();
 		
+		Connection con = null;
+		PreparedStatement preparedStatement = null;
 		
+		con = ConnectionUtil.getConnection();
+		String query = "insert into nonpatients(nonpatient_id, given_name, middle_name, last_name, "
+				+ "organization, date_of_birth, description, ward_id, staff_position_id, specialty_id) values (?,?,?,?,?,?,?,?,?,?)";
+		
+		try {
+			preparedStatement = con.prepareStatement(query);
+			preparedStatement.setString(1, id);
+			preparedStatement.setString(2, givenName);
+			preparedStatement.setString(3, middleName);
+			preparedStatement.setString(4, lastName);
+			preparedStatement.setString(5, organization);
+			preparedStatement.setDate(6, Date.valueOf(dateOfBirth));
+			preparedStatement.setString(7, description);
+			preparedStatement.setString(8, wardId);
+			preparedStatement.setString(9, staffPositionId);
+			preparedStatement.setString(10, specialtyId);
+			
+			int i = preparedStatement.executeUpdate();
+			if(i != 0) return NONPATIENTDAO_SUCCESS;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		return "";
 	}
