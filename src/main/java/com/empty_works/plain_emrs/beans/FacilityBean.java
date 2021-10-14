2155bf0d7d81fcf16fb0160dd0bde4f3fa291934
empty-works <pama.mark@gmail.com>
2021-10-14 22:30:49 +0200

    Created Facility bean.

---
+++ src/main/java/com/empty_works/plain_emrs/beans/FacilityBean.java
@@ -0,0 +1,55 @@
+package com.empty_works.plain_emrs.beans;
+
+public class FacilityBean {
+
+	String facilityId;
+	String name;
+	String streetAddress;
+	String city;
+	String state;
+	String country;
+	String zipCode;
+
+	public String getFacilityId() {
+		return facilityId;
+	}
+	public void setFacilityId(String facilityId) {
+		this.facilityId = facilityId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+}
--- src/main/java/com/empty_works/plain_emrs/beans/HospitalBean.java
+++
@@ -1,5 +0,0 @@
-package com.empty_works.plain_emrs.beans;
-
-public class HospitalBean {
-	//TODO: update data model to include hospital table and connect ward etc.
-}
