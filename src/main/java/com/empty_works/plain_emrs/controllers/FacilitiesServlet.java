ffa564825afdc2253349359f65d05f2753b4b32b
empty-works <pama.mark@gmail.com>
2021-10-22 22:46:49 +0200

    Created Facilities servlet and jsp.

---
+++ src/main/java/com/empty_works/plain_emrs/controllers/FacilitiesServlet.java
@@ -0,0 +1,35 @@
+package com.empty_works.plain_emrs.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FacilitiesServlet
+ */
+@WebServlet("/FacilitiesServlet")
+public class FacilitiesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@Resource(name="jdbc/plain_emrs")
+	private DataSource dataSource;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FacilitiesServlet() {
+        super();
+
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+}
