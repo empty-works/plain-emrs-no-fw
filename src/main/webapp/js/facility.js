44d7d9c1f0dd7b5e393371f99e2288a1c2ae2c49
empty-works <pama.mark@gmail.com>
2021-10-30 22:14:14 +0200

    Created js and css files for the add facility form.

---
+++ src/main/webapp/js/facility.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+ 
+ function showAddWardForm() {
+	
+	// Check if button is still hidden
+	if(document.getElementById("").innerHTML.includes("Add")) {
+		
+		
+	}
+}
+ document.getElementById("addWardForm").style.display = "none";
+ document.getElementById("addWardForm").style.display = "block";
+ 
