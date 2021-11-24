8a33524ab681b7e40da5c517914c5d7b993b373a
empty-works <pama.mark@gmail.com>
2021-11-24 18:11:47 +0100

    Created bean for staff specialty.

---
+++ src/main/java/com/empty_works/plain_emrs/beans/FacilityStaffSpecialtyBean.java
@@ -0,0 +1,34 @@
+package com.empty_works.plain_emrs.beans;
+
+public class FacilityStaffSpecialtyBean {
+
+	String facilityId;
+	String specialtyId;
+	String name;
+	String description;
+
+	public String getFacilityId() {
+		return facilityId;
+	}
+	public void setFacilityId(String facilityId) {
+		this.facilityId = facilityId;
+	}
+	public String getId() {
+		return specialtyId;
+	}
+	public void setId(String specialtyId) {
+		this.specialtyId = specialtyId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
