5b837fcb456eaf9fb4320c3473ee3279904e1872
empty-works <pama.mark@gmail.com>
2021-11-25 21:23:34 +0100

    Created ID generator wrapper for staff specialty.

---
+++ src/main/java/com/empty_works/plain_emrs/util/FacilitySpecialtyUtil.java
@@ -0,0 +1,22 @@
+package com.empty_works.plain_emrs.util;
+
+import com.empty_works.plain_emrs.beans.FacilityStaffSpecialtyBean;
+import com.empty_works.plain_emrs.util.helpers.IdUtil;
+
+public class FacilitySpecialtyUtil {
+
+	final public static String get(FacilityStaffSpecialtyBean fssb) {
+		
+		String id;
+		if(fssb.getDescription().equals("")) {
+			
+			id = IdUtil.get(fssb.getName(), "SPECIALITY_DESCRIPTION");
+		}
+		else {
+			
+			id = IdUtil.get(fssb.getName(), fssb.getDescription());
+		}
+		
+		return id;
+	}
+}
