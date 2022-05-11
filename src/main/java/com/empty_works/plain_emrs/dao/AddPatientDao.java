80499b3d2865923cea7c675136b6500ebcc2bc03
empty-works <pama.mark@gmail.com>
2022-05-11 04:53:28 +0200

    Work done on patient DAO.

--- src/main/java/com/empty_works/plain_emrs/dao/AddPatientDao.java
+++ src/main/java/com/empty_works/plain_emrs/dao/AddPatientDao.java
@@ -1,16 +1,20 @@
 package com.empty_works.plain_emrs.dao;
 
+import java.time.LocalDate;
+
 import com.empty_works.plain_emrs.beans.PatientBean;
 
-public class RegisterPatientDao {
+public class AddPatientDao {
 
 	//TODO: write to database. First, update database
 	public String register(PatientBean pb) {
 		
 		String id = pb.getId();
 		String givenName = pb.getGivenName();
-		String middleName = pb.getMiddleName();
+		String middleInitial = pb.getMiddleInitial();
 		String lastName = pb.getLastName();
+		LocalDate dateOfBirth = pb.getDateOfBirth(); 
+		
 		
 		return "";
 	}
