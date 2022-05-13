addb2561c385069d76ddcba0da24430ba16060a6
empty-works <pama.mark@gmail.com>
2022-05-13 02:31:51 +0200

    JUnit to test get method of the query utility.

--- src/test/java/com/empty_works/plain_emrs/util/QueryUtilityTest.java
+++ src/test/java/com/empty_works/plain_emrs/util/QueryUtilityTest.java
@@ -2,12 +2,18 @@ package com.empty_works.plain_emrs.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class QueryUtilityTest {
 
 	@Test
 	void testGet() {
-		fail("Not yet implemented");
+		
+		String query = "insert into patients("
+				+ "given_name,middle_initial,last_name)"
+				+ " values (?,?,?)";
+		String producedQuery = QueryUtility.get("patients", "given_name", "middle_initial", "last_name");
+		Assertions.assertEquals(query, producedQuery);
 	}
 }
