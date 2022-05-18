0f0e90c4dfeb5375aa1aec281e4126513bfd824f
empty-works <pama.mark@gmail.com>
2022-05-18 20:41:46 +0200

    Fixed issue with casting and retrieving local date from the result set.

--- src/main/java/com/empty_works/plain_emrs/dao/PatientDao.java
+++ src/main/java/com/empty_works/plain_emrs/dao/PatientDao.java
@@ -1,10 +1,12 @@
 package com.empty_works.plain_emrs.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.io.InputStream;
 
 import com.empty_works.plain_emrs.beans.PatientBean;
 import com.empty_works.plain_emrs.util.ConnectionUtil;
@@ -19,19 +21,27 @@ public class PatientDao {
 		Connection con = ConnectionUtil.getConnection();
 		PreparedStatement preparedStatement = null;
 		
-		String query = QueryUtil.getWithCondition("patients", "patient_id", "patient_id", "patient_given_name", "patient_middle_initial", 
+		String query = QueryUtil.getWithCondition("patients", "patient_id", "patient_given_name", "patient_middle_initial", 
 				"patient_last_name", "patient_date_of_birth", "patient_provider", "patient_provider_id", "patient_room", "patient_gender", 
 				"patient_type", "patient_race", "patient_ethnicity", "patient_language_preference", "patient_street_address", "patient_city", 
 				"patient_state", "patient_country", "patient_phone_number", "patient_facility_id");
 		
 		System.out.println("Patient get query: " + query);
 		
-		PatientBean pb = new PatientBean();
+		PatientBean patient = new PatientBean();
 		try {
 
 			preparedStatement = con.prepareStatement(query);
 			preparedStatement.setString(1, patientId);
 			ResultSet resultSet = preparedStatement.executeQuery();
+			// Check if the result set is empty.
+			if(resultSet.next()) {InputStream stream = resultSet.getBinaryStream(1);}
+			
+			patient.setId(patientId);
+			patient.setGivenName(resultSet.getString("patient_given_name"));
+			patient.setMiddleInitial(resultSet.getString("patient_middle_initial"));
+			patient.setLastName(resultSet.getString("patient_last_name"));
+			patient.setDateOfBirth(resultSet.getObject("patient_date_of_birth", LocalDate.class));
 			
 		} catch (SQLException e) {
 
