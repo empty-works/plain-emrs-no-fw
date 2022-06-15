d351b255425edca00e0d6102df6ff72532a69e0f
empty-works <pama.mark@gmail.com>
2022-06-15 22:15:24 +0200

    Made test for the select query creator class.

---
+++ src/test/java/com/empty_works/plain_emrs/util/SelectQueryCreatorTest.java
@@ -0,0 +1,73 @@
+package com.empty_works.plain_emrs.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.empty_works.plain_emrs.util.helpers.QueryCondition;
+import com.empty_works.plain_emrs.util.helpers.QueryDataType;
+import com.empty_works.plain_emrs.util.helpers.QueryField;
+import com.empty_works.plain_emrs.util.helpers.QueryJoin;
+
+class SelectQueryCreatorTest {
+
+	@Test
+	void testSelectNoJoinNoConditions() {
+		
+		String testQuery = "SELECT user_name,user_home_address,user_email_address FROM users";
+		QueryUtil qu = new QueryUtil(""); // Dummy constructor
+		qu.addField(new QueryField(QueryDataType.STRING, "user_name"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_home_address"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_email_address"));
+		qu.setTable("users");
+		String finalQuery = qu.select();
+		System.out.println("Select final query: " + finalQuery);
+		Assertions.assertEquals(testQuery, finalQuery);
+	}
+	
+	@Test
+	void testSelectWithJoin() {
+		
+		String testQuery = "SELECT user_name,user_home_address,user_email_address FROM users JOIN authorities ON users.user_name=authorities.user_name";
+		QueryUtil qu = new QueryUtil(""); // Dummy constructor
+		qu.addField(new QueryField(QueryDataType.STRING, "user_name"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_home_address"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_email_address"));
+		qu.setTable("users");
+		qu.setJoin(new QueryJoin(QueryJoin.JOIN, "authorities", "users.user_name=authorities.user_name"));
+		String finalQuery = qu.select();
+		System.out.println("Select final query with join: " + finalQuery);
+		Assertions.assertEquals(testQuery, finalQuery);
+	}
+	
+	@Test
+	void testSelectWithOneCondition() {
+		
+		String testQuery = "SELECT user_home_address,user_email_address FROM users WHERE user_name=?";
+		QueryUtil qu = new QueryUtil(""); // Dummy constructor
+		qu.addField(new QueryField(QueryDataType.STRING, "user_home_address"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_email_address"));
+		qu.setTable("users");
+		qu.addCondition(new QueryCondition(QueryCondition.FIRST, "user_name=?"));
+		String finalQuery = qu.select();
+		System.out.println("Select final query with a condition: " + finalQuery);
+		Assertions.assertEquals(testQuery, finalQuery);
+	}
+	
+	@Test
+	void testSelectWithConditions() {
+		
+		String testQuery = "SELECT user_name,user_home_address,user_email_address FROM users WHERE user_home_address=? AND user_email_address=?";
+		QueryUtil qu = new QueryUtil(""); // Dummy constructor
+		qu.addField(new QueryField(QueryDataType.STRING, "user_name"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_home_address"));
+		qu.addField(new QueryField(QueryDataType.STRING, "user_email_address"));
+		qu.setTable("users");
+		qu.addCondition(new QueryCondition(QueryCondition.FIRST, "user_home_address=?"));
+		qu.addCondition(new QueryCondition(QueryCondition.AND, "user_email_address=?"));
+		String finalQuery = qu.select();
+		System.out.println("Select final query with conditions: " + finalQuery);
+		Assertions.assertEquals(testQuery, finalQuery);
+	}
+}
