19b32a6fe4578fe93a350715cb572117a878bbf6
empty-works <pama.mark@gmail.com>
2022-07-27 22:34:11 +0200

    Simply alphabetized the medical problems/surgical procedures lists.

--- src/main/java/com/empty_works/plain_emrs/patient_choices/PatientMedicalProblemLists.java
+++ src/main/java/com/empty_works/plain_emrs/patient_choices/PatientMedicalProblemLists.java
@@ -12,33 +12,33 @@ public class PatientMedicalProblemLists {
 		private static final long serialVersionUID = 4002195277166548304L;
 
 		{
-			add(new PatientMedicalProblemUnit("medProbEyes", "Eyes"));
-			add(new PatientMedicalProblemUnit("medProbEars", "Ears"));
-			add(new PatientMedicalProblemUnit("medProbNose", "Nose"));
-			add(new PatientMedicalProblemUnit("medProbSinuses", "Sinuses"));
-			add(new PatientMedicalProblemUnit("medProbTonsils", "Tonsils"));
-			add(new PatientMedicalProblemUnit("medProbThyParaThyGland", "Thyroid or parathyroid gland"));
-			add(new PatientMedicalProblemUnit("medProbLungs", "Lungs"));
-			add(new PatientMedicalProblemUnit("medProbEsoph", "Esophagus"));
-			add(new PatientMedicalProblemUnit("medProbStoma", "Stomach"));
-			add(new PatientMedicalProblemUnit("medProbBowel", "Bowel"));
 			add(new PatientMedicalProblemUnit("medProbAppend", "Appendix"));
-			add(new PatientMedicalProblemUnit("medProbLymph", "Lymph nodes"));
-			add(new PatientMedicalProblemUnit("medProbSpleen", "Spleen"));
-			add(new PatientMedicalProblemUnit("medProbLiver", "Liver"));
-			add(new PatientMedicalProblemUnit("medProbGallBl", "Gallbladder"));
-			add(new PatientMedicalProblemUnit("medProbPancre", "Pancreas"));
-			add(new PatientMedicalProblemUnit("medProbHernia", "Hernia"));
-			add(new PatientMedicalProblemUnit("medProbKidneys", "Kidneys"));
+			add(new PatientMedicalProblemUnit("medProbBack", "Back"));
 			add(new PatientMedicalProblemUnit("medProbBladder", "Bladder"));
 			add(new PatientMedicalProblemUnit("medProbBones", "Bones"));
-			add(new PatientMedicalProblemUnit("medProbJoints", "Joints"));
-			add(new PatientMedicalProblemUnit("medProbMuscles", "Muscles"));
-			add(new PatientMedicalProblemUnit("medProbBack", "Back"));
-			add(new PatientMedicalProblemUnit("medProbNeck", "Neck"));
-			add(new PatientMedicalProblemUnit("medProbSpine", "Spine"));
+			add(new PatientMedicalProblemUnit("medProbBowel", "Bowel"));
 			add(new PatientMedicalProblemUnit("medProbBrain", "Brain"));
+			add(new PatientMedicalProblemUnit("medProbEars", "Ears"));
+			add(new PatientMedicalProblemUnit("medProbEsoph", "Esophagus"));
+			add(new PatientMedicalProblemUnit("medProbEyes", "Eyes"));
+			add(new PatientMedicalProblemUnit("medProbGallBl", "Gallbladder"));
+			add(new PatientMedicalProblemUnit("medProbHernia", "Hernia"));
+			add(new PatientMedicalProblemUnit("medProbJoints", "Joints"));
+			add(new PatientMedicalProblemUnit("medProbKidneys", "Kidneys"));
+			add(new PatientMedicalProblemUnit("medProbLiver", "Liver"));
+			add(new PatientMedicalProblemUnit("medProbLungs", "Lungs"));
+			add(new PatientMedicalProblemUnit("medProbLymph", "Lymph nodes"));
+			add(new PatientMedicalProblemUnit("medProbMuscles", "Muscles"));
+			add(new PatientMedicalProblemUnit("medProbNeck", "Neck"));
+			add(new PatientMedicalProblemUnit("medProbNose", "Nose"));
+			add(new PatientMedicalProblemUnit("medProbPancre", "Pancreas"));
+			add(new PatientMedicalProblemUnit("medProbSinuses", "Sinuses"));
 			add(new PatientMedicalProblemUnit("medProbSkin", "Skin"));
+			add(new PatientMedicalProblemUnit("medProbSpine", "Spine"));
+			add(new PatientMedicalProblemUnit("medProbSpleen", "Spleen"));
+			add(new PatientMedicalProblemUnit("medProbStoma", "Stomach"));
+			add(new PatientMedicalProblemUnit("medProbTonsils", "Tonsils"));
+			add(new PatientMedicalProblemUnit("medProbThyParaThyGland", "Thyroid or parathyroid gland"));
 		}
 	};
 	
@@ -49,13 +49,13 @@ public class PatientMedicalProblemLists {
 		private static final long serialVersionUID = 1508226257245976911L;
 
 		{
-			add(new PatientMedicalProblemUnit("medProbHeAtt", "Heart attack"));
-			add(new PatientMedicalProblemUnit("medProbHeVal", "Heart valves"));
 			add(new PatientMedicalProblemUnit("medProbAbnHeRhy", "Abnormal heart rhythm"));
-			add(new PatientMedicalProblemUnit("medProbNarrCoroArt", "Narrowed coronary arteries"));
 			add(new PatientMedicalProblemUnit("medProbArteries", "Arteries (head, arms, legs, aorta, etc.)"));
-			add(new PatientMedicalProblemUnit("medProbBlooClots", "Veins or blood clots in the veins"));
+			add(new PatientMedicalProblemUnit("medProbHeAtt", "Heart attack"));
 			add(new PatientMedicalProblemUnit("medProbHeartOther", "Heart other"));
+			add(new PatientMedicalProblemUnit("medProbHeVal", "Heart valves"));
+			add(new PatientMedicalProblemUnit("medProbNarrCoroArt", "Narrowed coronary arteries"));
+			add(new PatientMedicalProblemUnit("medProbBlooClots", "Veins or blood clots in the veins"));
 		}
 	};
 	
@@ -67,14 +67,14 @@ public class PatientMedicalProblemLists {
 
 		{
 			add(new PatientMedicalProblemUnit("medProbBreasts", "Breasts"));
-			add(new PatientMedicalProblemUnit("medProbUterus", "Uterus"));
-			add(new PatientMedicalProblemUnit("medProbOvaries", "Ovaries"));
 			add(new PatientMedicalProblemUnit("medProbFallTubes", "Fallopian tubes"));
 			add(new PatientMedicalProblemUnit("medProbHysterec", "Hysterectomy"));
-			add(new PatientMedicalProblemUnit("medProbReproductOther", "Reproductive other"));
-			add(new PatientMedicalProblemUnit("medProbProstate", "Prostate"));
+			add(new PatientMedicalProblemUnit("medProbOvaries", "Ovaries"));
 			add(new PatientMedicalProblemUnit("medProbPenis", "Penis"));
+			add(new PatientMedicalProblemUnit("medProbProstate", "Prostate"));
+			add(new PatientMedicalProblemUnit("medProbReproductOther", "Reproductive other"));
 			add(new PatientMedicalProblemUnit("medProbTesticles", "Testicles"));
+			add(new PatientMedicalProblemUnit("medProbUterus", "Uterus"));
 			add(new PatientMedicalProblemUnit("medProbVasect", "Vasectomy"));
 		}
 	};
