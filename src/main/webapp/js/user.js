47d8c8e4cf454356025526c57cd25c3d9f5cd2dd
empty-works <pama.mark@gmail.com>
2022-08-17 22:52:35 +0200

    The remove button for each node is now added as a new element instead of as adjacent HTML.

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -32,54 +32,6 @@ function showNonpatientForm() {
 	nonpatientSection.style.display = "block";
 }
 
-function addMedProblem(jsonList, medProbCon) {
-	
-	let node = new MedProbAdder(jsonList, medProbCon);
-	node.addNode();
-	/*
-	if(medProbLimit < MED_PROB_MAX) {
-		medProbLimit++;
-		console.log(jsonList);
-		let medProbContainer = medProbCon;
-		let medProbNode = document.createElement("div");
-		let medProbDropDown = document.createElement("select");
-		let medProbList = [];
-		// Push JSON elements to a JS list
-		for(let x in jsonList) {
-			medProbList.push(x, jsonList[x]);
-		}
-		// Sort the JS list
-		medProbList.sort();
-		// Then add the sorted list elements to a dropdown menu
-		for(let x in medProbList) {
-			console.log(medProbList[x]);
-			let element = medProbList[x];
-			let option = document.createElement("option");
-			option.textContent = element;
-			option.value = element; 
-			medProbDropDown.appendChild(option);
-		}
-		// Add the rest of the medical problem inputs to the node
-		let medProbHtml = 
-		"<input type=\"text\" class=\"med-prob-text\" name=\"medProbText\" placeholder=\"Medical problem:\">" + 
-		"<input type=\"text\" class=\"med-prob-text\" name=\"medProbSurgeryText\" placeholder=\"Surgical procedure:\">" + 
-		"<label class=\"med-prob-text\" for=\"medProbSurgeryDate\">Surgical procedure date: </label>" + 
-		"<input type=\"date\" class=\"medProbSurgeryDate\" name=\"medProbSurgeryDate\">" + 
-		"<button type=\"button\" onclick=\"removeMedProbNode(this)\">-</button>";
-		medProbNode.appendChild(medProbDropDown);
-		medProbNode.insertAdjacentHTML("beforeend", medProbHtml);
-		medProbContainer.appendChild(medProbNode);
-	}
-	else {
-		const limitMsg = document.getElementById("addLimitReached");
-		let text = "Cannot add more!";
-		// Clear any text
-		limitMsg.innerHTML = "";
-		limitMsg.innerHTML = text;
-	}
-	*/
-}
-
 class MedProbAdder {
 	constructor() {
 		this.medProbLimit = 0;
@@ -113,24 +65,29 @@ class MedProbAdder {
 			"<input type=\"text\" class=\"med-prob-text\" name=\"medProbText\" placeholder=\"Medical problem:\">" + 
 			"<input type=\"text\" class=\"med-prob-text\" name=\"medProbSurgeryText\" placeholder=\"Surgical procedure:\">" + 
 			"<label class=\"med-prob-text\" for=\"medProbSurgeryDate\">Surgical procedure date: </label>" + 
-			"<input type=\"date\" class=\"medProbSurgeryDate\" name=\"medProbSurgeryDate\">" + 
-			"<button type=\"button\" onclick=\"removeMedProbNode(this)\">-</button>";
+			"<input type=\"date\" class=\"medProbSurgeryDate\" name=\"medProbSurgeryDate\">" 
 			medProbNode.appendChild(medProbDropDown);
 			medProbNode.insertAdjacentHTML("beforeend", medProbHtml);
+			let removeButton = document.createElement("button");
+			removeButton.innerHTML = "-";
+			removeButton.onclick = function() {
+				removeMedProbNode(medProbNode);	
+			};
+			medProbNode.appendChild(removeButton);
 			medProbCon.appendChild(medProbNode);
 		}
 		else {
-			const limitMsg = document.getElementById("addLimitReached");
+			let limitMsg = document.createElement("div");	
 			let text = "Cannot add more!";
 			// Clear any text
 			limitMsg.innerHTML = "";
 			limitMsg.innerHTML = text;
+			medProbCon.appendChild(limitMsg);
 		}
 	}
 
 	removeMedProbNode(medProbNode) {
-		const limitMsg = document.getElementById("addLimitReached");
-		limitMsg.innerHTML = "";
+		this.medProbCon.removeChild(this.medProbCon.lastChild);
 		medProbNode.parentElement.remove();		
 		this.medProbLimit--;
 	}
@@ -140,3 +97,6 @@ genMed = new MedProbAdder();
 heartMed = new MedProbAdder();
 reproductMed = new MedProbAdder();
 
+function removeMedProbNodeWrap(med, medProbNode) {
+	med.removeMedProbNode(medProbNode);
+}
