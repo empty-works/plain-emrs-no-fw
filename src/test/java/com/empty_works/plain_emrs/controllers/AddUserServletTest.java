2be654a5d81caad021f830ead88a6950be0b13cb
empty-works <pama.mark@gmail.com>
2022-09-21 23:03:34 +0200

    Created test for parsing surgeries in the add user servlet.

--- src/test/java/com/empty_works/plain_emrs/controllers/AddUserServletTest.java
+++ src/test/java/com/empty_works/plain_emrs/controllers/AddUserServletTest.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 
 import com.empty_works.plain_emrs.patient_choices.MedicalRecordDiseaseUnit;
+import com.empty_works.plain_emrs.patient_choices.MedicalRecordSurgeryUnit;
 
 public class AddUserServletTest {
 
@@ -67,5 +68,56 @@ public class AddUserServletTest {
 		}
 		System.out.println(result);
 		assertIterableEquals(expected, result);
+	}
+	
+	@Test
+	void testParseSurgeries_threeProbAreas_threeSurgRelatedProb_threeSurgProcedure_threeSurgYears() {
+		
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		String[] problemAreas = {"Bladder", "Brain", "Heart valves"}; 
+		String[] surgeryRelatedProblems = {"Overactive bladder", "Brain tumor", "Stenosis"};
+		String[] surgicalProcedures = {"Urinary diversion", "Awake craniotomy", "Valvuloplasty"};
+		String[] surgicalProceduresYears = {"1994", "2011", "1991"};
+		when(request.getParameterValues("medProbArea")).thenReturn(problemAreas);
+		when(request.getParameterValues("medProbText")).thenReturn(surgeryRelatedProblems);
+		when(request.getParameterValues("medProbSurgeryText")).thenReturn(surgicalProcedures);
+		when(request.getParameterValues("medProbSurgeryDate")).thenReturn(surgicalProceduresYears);
+		
+		List<String> probAreaExpected = new ArrayList<>();
+		probAreaExpected.add("Bladder");
+		probAreaExpected.add("Brain");
+		probAreaExpected.add("Heart valves");
+		List<String> surgRelatedProbsExpected = new ArrayList<>();
+		surgRelatedProbsExpected.add("Overactive bladder");
+		surgRelatedProbsExpected.add("Brain tumor");
+		surgRelatedProbsExpected.add("Stenosis");
+		List<String> surgProceduresExpected = new ArrayList<>();
+		surgProceduresExpected.add("Urinary diversion");
+		surgProceduresExpected.add("Awake craniotomy");
+		surgProceduresExpected.add("Valvuloplasty");
+		List<String> surgProceduresYearsExpected = new ArrayList<>();
+		surgProceduresYearsExpected.add("1994");
+		surgProceduresYearsExpected.add("2011");
+		surgProceduresYearsExpected.add("1991");
+		List<String> probAreaResult = new ArrayList<>();
+		List<String> surgRelatedResult = new ArrayList<>();
+		List<String> surgProceduresResult = new ArrayList<>();
+		List<String> surgProceduresYearsResult = new ArrayList<>();
+		List<MedicalRecordSurgeryUnit> parsedMedProblems = AddUserServlet.parseSurgeries(request);
+		for(MedicalRecordSurgeryUnit parsedMedProblem : parsedMedProblems) {
+			
+			probAreaResult.add(parsedMedProblem.getProblemArea());
+			surgRelatedResult.add(parsedMedProblem.getSurgicalRelatedProblem());
+			surgProceduresResult.add(parsedMedProblem.getSurgicalProcedure());
+			surgProceduresYearsResult.add(parsedMedProblem.getSurgicalProcedureYear());
+		}
+		System.out.println(probAreaResult);
+		System.out.println(surgRelatedResult);
+		System.out.println(surgProceduresResult);
+		System.out.println(surgProceduresYearsResult);
+		assertIterableEquals(probAreaExpected, probAreaResult);
+		assertIterableEquals(surgRelatedProbsExpected, surgRelatedResult);
+		assertIterableEquals(surgProceduresExpected, surgProceduresResult);
+		assertIterableEquals(surgProceduresYearsExpected, surgProceduresYearsResult);
 	}
 }
