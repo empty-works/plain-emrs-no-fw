3bc0d808b3f2b41042f46a3c63ece50f0daf2f9c
empty-works <pama.mark@gmail.com>
2022-11-10 19:38:53 +0100

    Fixed diseases bean so diseases are added properly.

--- src/main/java/com/empty_works/plain_emrs/beans/DiseasesBean.java
+++ src/main/java/com/empty_works/plain_emrs/beans/DiseasesBean.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import com.empty_works.plain_emrs.patient_choices.MedicalRecordDiseaseUnit;
 
-public class DiseasesBean implements PatientIdInterface, BeanDaoInterface {
+public class DiseasesBean implements BeanDaoInterface {
 
 	private String medicalRecordId;
 	private List<MedicalRecordDiseaseUnit> diseases;
@@ -35,12 +35,12 @@ public class DiseasesBean implements PatientIdInterface, BeanDaoInterface {
 	@Override
 	public int prepareStatments(PreparedStatement preparedStatement) throws SQLException {
 
-		for(int i = 0; i < diseases.getDiseases().size(); i++) {
+		for(int i = 0; i < diseases.size(); i++) {
 			
-			preparedStatement.setString(1, diseases.get(i).getMedicalRecordId());
-			preparedStatement.setString(2, diseases.getDiseases().get(i).getDiseaseName());
-			preparedStatement.setBoolean(3, diseases.getDiseases().get(i).isContractedDisease());
-			preparedStatement.setBoolean(4, diseases.getDiseases().get(i).isImmunized());
+			preparedStatement.setString(1, medicalRecordId);
+			preparedStatement.setString(2, diseases.get(i).getDiseaseName());
+			preparedStatement.setBoolean(3, diseases.get(i).isContractedDisease());
+			preparedStatement.setBoolean(4, diseases.get(i).isImmunized());
 			preparedStatement.addBatch();
 		}
 		return preparedStatement.executeBatch()[0];
