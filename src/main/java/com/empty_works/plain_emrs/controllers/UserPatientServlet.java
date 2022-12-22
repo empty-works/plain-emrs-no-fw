603d6a3914dda58f08f8b969475d3b106872636f
empty-works <pama.mark@gmail.com>
2022-12-22 23:11:19 +0100

    Set attributes for the user patient overview data in the user patient servlet.

--- src/main/java/com/empty_works/plain_emrs/controllers/UserPatientServlet.java
+++ src/main/java/com/empty_works/plain_emrs/controllers/UserPatientServlet.java
@@ -28,7 +28,29 @@ public class UserPatientServlet extends HttpServlet {
 		System.out.println("userPatientId: " + userPatientId);
 		request.setAttribute("userPatientId", userPatientId);
 		PatientBean patient = UserPatientDao.getPatient(userPatientId);
-		
+		request.setAttribute("patientProvider", patient.getProvider());
+		request.setAttribute("patientProviderId", patient.getProviderId());
+		request.setAttribute("patientRoom", patient.getRoomNumber());
+		request.setAttribute("patientType", patient.getType());
+		request.setAttribute("patientLanguagePreference", patient.getLanguagePreference());
+		request.setAttribute("patientStreetAddress", patient.getStreetAddress());
+		request.setAttribute("patientCity", patient.getCity());
+		request.setAttribute("patientState", patient.getState());
+		request.setAttribute("patientCountry", patient.getCountry());
+		request.setAttribute("patientPhoneNumber", patient.getPhoneNumber());
+		request.setAttribute("patientBirthGender", patient.getGenderAtBirth());
+		request.setAttribute("userEmailAddress", patient.getEmailAddress());
+		request.setAttribute("userEnabled", patient.isUserEnabled());
+		request.setAttribute("userCreatedOn", patient.getDateCreated());
+		request.setAttribute("userCurrentFacilityId", patient.getCurrentFacilityId());
+		request.setAttribute("userPatientFirstName", patient.getFirstName());
+		request.setAttribute("userPatientMiddleInitial", patient.getMiddleInitial());
+		request.setAttribute("userPatientLastName", patient.getLastName());
+		request.setAttribute("userDateOfBirth", patient.getDateOfBirth());
+		request.setAttribute("emergencyContactGivenName", patient.getEmergencyContactGivenName());
+		request.setAttribute("emergencyContactLastName", patient.getEmergencyContactLastName());
+		request.setAttribute("emergencyContactPhoneNumber", patient.getEmergencyContactPhoneNumber());
+		request.setAttribute("emergencyContactEmail", patient.getEmergencyContactEmail());
 		request.getRequestDispatcher("/WEB-INF/UserPatient.jsp").forward(request, response);
 	}
 
