e709e583b8b4f1c46c6dc0a9132b7efa9ae604a7
empty-works <pama.mark@gmail.com>
2023-01-03 15:13:21 +0100

    Emergency contacts bean.

---
+++ src/main/java/com/empty_works/plain_emrs/beans/EmergencyContactsBean.java
@@ -0,0 +1,37 @@
+package com.empty_works.plain_emrs.beans;
+
+public class EmergencyContactsBean {
+
+	private String firstName, middleInitial, lastName;
+	private String phoneNumber, email;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
